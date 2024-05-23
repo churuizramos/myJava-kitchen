94d411ee4c3153f9af006dfa5ec5bdcc73019a8e
jRuzzy <ruizramos04@gmail.com>
2024-05-23 09:36:07 -0400

    Copied recurring code to an external function in the main class
    It takes in a string for the letter and the variables are saved as part of an array.

--- Lab1c/src/Main.java
+++ Lab1c/src/Main.java
@@ -1,36 +1,23 @@
 import java.util.*;
 
 public class Main {
+    private static void requestValue(String letter, double [] array, int position) {
+        do {
+            System.out.print("Enter side " + letter + ": ");
+            Scanner sc = new Scanner(System.in);
+            array[position-1] = sc.nextDouble();
+            if (array[position-1] < 1) {
+                System.out.println("!Must be greater than 0!");
+            }
+        } while (array[position-1] < 1);
+    }
     public static void main(String[] args) {
-        double a, b, c;
+        double[] sidesArray = new double[3];
 
-        do {
-            System.out.print("Enter side a: ");
-            Scanner sc = new Scanner(System.in);
-            a = sc.nextDouble();
-            if (a < 1) {
-                System.out.println("!Must be greater than 0!");
-            }
-        } while (a < 1);
+        requestValue("a", sidesArray, 1);
+        requestValue("b", sidesArray, 2);
+        requestValue("c", sidesArray, 3);
 
-        do {
-            System.out.print("Enter side a: ");
-            Scanner sc = new Scanner(System.in);
-            b = sc.nextDouble();
-            if (b < 1) {
-                System.out.println("!Must be greater than 0!");
-            }
-        } while (b < 1);
-
-        do {
-            System.out.print("Enter side a: ");
-            Scanner sc = new Scanner(System.in);
-            c = sc.nextDouble();
-            if (c < 1) {
-                System.out.println("!Must be greater than 0!");
-            }
-        } while (c < 1);
-
-        Triangle t = new Triangle(a, b, c);
+        Triangle t = new Triangle(sidesArray[0], sidesArray[1], sidesArray[2]);
     }
 }
