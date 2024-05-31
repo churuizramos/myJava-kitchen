37a5d5d632ff19b729cf526a235c956a0c887b0b
jRuzzy <ruizramos04@gmail.com>
2024-05-31 11:05:49 -0400

    Updated the TriangleKB class to correctly represent the instructions

--- RuizRamos_Jesus_1c/src/TriangleKBInput.java
+++ RuizRamos_Jesus_1c/src/TriangleKBInput.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
 public class TriangleKBInput {
-    public TriangleKBInput(double a, double b, double c) {
+    public TriangleKBInput() {
+        double[] sides = new double[3];
+
+        requestValue("a", sides, 0);
+        requestValue("b", sides, 1);
+        requestValue("c", sides, 2);
+
         System.out.println("The triangle has:");
-        double semiPerimeter = ((a + b + c)*0.5);
+        double semiPerimeter = ((sides[0] + sides[1] + sides[2])*0.5);
         System.out.println("Semiperimeter: " + semiPerimeter);
-        System.out.println("Area: " + Math.sqrt(semiPerimeter*(semiPerimeter-a)*(semiPerimeter-b)*(semiPerimeter-c)));
+        System.out.println("Area: " + Math.sqrt(semiPerimeter*(semiPerimeter-sides[0])*(semiPerimeter-sides[1])*(semiPerimeter-sides[2])));
     }
-//    private static void requestValue(String letter, double [] array, int position) {
-//        do {
-//            System.out.print("Enter side " + letter + ": ");
-//            Scanner sc = new Scanner(System.in);
-//            array[position-1] = sc.nextDouble();
-//            if (array[position-1] < 1) {
-//                System.out.println("!Must be greater than 0!");
-//            }
-//        } while (array[position-1] < 1);
-//    }
+    private static void requestValue(String letter, double [] array, int position) {
+        do {
+            System.out.print("Enter side " + letter + ": ");
+            Scanner sc = new Scanner(System.in);
+            array[position] = sc.nextDouble();
+            if (array[position] < 1) {
+                System.out.println("!Must be greater than 0!");
+            }
+        } while (array[position] < 1);
+    }
 }
