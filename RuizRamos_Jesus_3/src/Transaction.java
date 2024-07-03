727c525798f9b775f6f6f4adb848e3ef0c374c1f
churuizramos <ruizramos04@outlook.com>
2024-07-03 13:18:24 -0400

    Fixed documentation and commenting and removed unused testing methods

--- RuizRamos_Jesus_3/src/Transaction.java
+++ RuizRamos_Jesus_3/src/Transaction.java
@@ -10,6 +10,14 @@ public class Transaction {
 
     private Collector seller, buyer;
 
+    /**
+     * Creates a Transaction object to be stored in the TransactionDB class
+     * @param pieceName Name of the piece being sold
+     * @param seller The Collector object providing the piece
+     * @param buyer The Collector object receiving the piece
+     * @param sales double value representing the cost of the Transaction
+     * @param year integer value representing the date
+     */
     public Transaction(String pieceName, Collector seller, Collector buyer, double sales, int year) {
         this.pieceName = pieceName;
         this.seller = seller;
@@ -17,6 +25,11 @@ public class Transaction {
         this.sales = sales;
         this.year = year;
     }
+
+    /**
+     * Prints all the Transaction info as String
+     * @return String in format [pieceName sold by seller to buyer for $sales in year]
+     */
     public String toString() {
         DecimalFormat salesFormat = new DecimalFormat("0.00");
         return "[" + pieceName + " sold by " + seller + " to " + buyer + " for $" + salesFormat.format(sales) + " in " + year + "]";
@@ -25,12 +38,6 @@ public class Transaction {
     /**
      * Getters
      */
-    public String getBuyer() {
-        return buyer.toString();
-    }
-    public String getSeller() {
-        return seller.toString();
-    }
     public double getSales() {
         return sales;
     }
