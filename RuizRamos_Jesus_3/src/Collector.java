fe09c29f4dd0c1a67020ccc178bbbeb7897ec2cf
churuizramos <ruizramos04@outlook.com>
2024-07-03 13:13:35 -0400

    Fixed documentation and commenting on the Collector class

--- RuizRamos_Jesus_3/src/Collector.java
+++ RuizRamos_Jesus_3/src/Collector.java
@@ -6,14 +6,30 @@ public class Collector {
     private String colName;
     private char category; // C for commercial, N for nonprofit
 
+    /**
+     * Creates a Collector object to be used with a Transaction
+     * @param colID Identifying integer
+     * @param colName Name of the Collector
+     * @param category Category value
+     */
     public Collector(int colID, String colName, char category) {
         this.colID = colID;
         this.colName = colName;
         this.category = category;
     }
+
+    /**
+     * Getter for the Collector name variable
+     * @return String colName
+     */
     public String getColName() {
         return colName;
     }
+
+    /**
+     * Prints the collector info as a String
+     * @return String in format {ID: colID | Name: colName | Cat: category}
+     */
     public String toString() {
         return "{ID: " + colID + " | Name: " + colName + " | Cat: " + category + "}";
     }
