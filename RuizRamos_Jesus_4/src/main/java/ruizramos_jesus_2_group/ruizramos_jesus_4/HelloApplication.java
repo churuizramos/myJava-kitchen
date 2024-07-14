d39432efd2352acc70285896f8a7af1dd928f9d4
Jesus <ruizramos04@outlook.com>
2024-07-13 20:50:36 -0400

    update for sync

--- RuizRamos_Jesus_4/src/main/java/ruizramos_jesus_2_group/ruizramos_jesus_4/HelloApplication.java
+++ RuizRamos_Jesus_4/src/main/java/ruizramos_jesus_2_group/ruizramos_jesus_4/HelloApplication.java
@@ -13,6 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         Text helloWorld = new Text(50,50,"Hello World");
         Group root = new Group(helloWorld);
+
         Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
